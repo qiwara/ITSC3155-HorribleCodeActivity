9e996e8313285402f8654a1d4962c4020c0692e1
Qi <qye2@charlotte.edu>
2026-09-14 13:10:54 -0400

    feat: allow BadCalculator.py to perform calculations until exit

--- BadCalculator.py
+++ BadCalculator.py
@@ -3,16 +3,37 @@ def add():
     second_number = float(input("Please enter the second number: "))
     print(first_number + second_number)
 
+    again = input("Would you like to perform another addition? ")
+    while (again == "yes"):
+        first_number = float(input("Please enter the first number: "))
+        second_number = float(input("Please enter the second number: "))
+        print(first_number + second_number)
+        again = input("Would you like to perform another addition? ")
+
     result = first_number + second_number
 def subtract():
     first_number = float(input("Please enter the first number: "))
     second_number = float(input("Please enter the second number: "))
     print(first_number - second_number)
 
+    again = input("Would you like to perform another subtraction? ")
+    while (again == "yes"):
+        first_number = float(input("Please enter the first number: "))
+        second_number = float(input("Please enter the second number: "))
+        print(first_number - second_number)
+        again = input("Would you like to perform another subtraction? ")
+
 def multiply():
     first_number = float(input("Please enter the first number: "))
     second_number = float(input("Please enter the second number: "))
     print(first_number * second_number)
+
+    again = input("Would you like to perform another multiplication? ")
+    while (again == "yes"):
+        first_number = float(input("Please enter the first number: "))
+        second_number = float(input("Please enter the second number: "))
+        print(first_number * second_number)
+        again = input("Would you like to perform another multiplication? ")
 
 def divide():
     first_number = float(input("Please enter the first number: "))
@@ -22,26 +43,38 @@ def divide():
     else:
         print(first_number / second_number)
 
-print("====================================")
-print("             CALCULATOR 3000")
-print("====================================")
-print("1. Addition")
-print("2. Subtraction")
-print("3. Multiplication")
-print("4. Division")
-print("====================================")
+    again = input("Would you like to perform another division? ")
+    while (again == "yes"):
+        first_number = float(input("Please enter the first number: "))
+        second_number = float(input("Please enter the second number: "))
+        if second_number == 0:
+            print("You cannot divide by zero!")
+            again = input("Would you like to perform another division? ")
+        else:
+            print(first_number / second_number)
+            again = input("Would you like to perform another division? ")
 
-choice = input("Please select an operation: ")
-
-if choice == "1":
-    add()
-elif choice == "2":
-    subtract()
-elif choice == "3":
-    multiply()
-elif choice == "4":
-    divide()
-else:
-    print("Invalid selection.")
-
-print("Thank you for using the calculator!")
+while True:
+    print("====================================")
+    print("             CALCULATOR 3000")
+    print("====================================")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exit")
+    print("====================================")
+    choice = input("Please select an operation: ")
+    if choice == "1":
+     add()
+    elif choice == "2":
+      subtract()
+    elif choice == "3":
+      multiply()
+    elif choice == "4":
+        divide()
+    elif choice == "5":
+        print("Thank you for using the calculator!")
+        break
+    else:
+        print("Invalid selection, try again.")
